7da1173f4b71f6c0d41a6d4f82b551d3f37423a1
tyson-tyler <benword25@gmail.com>
2024-04-18 18:09:31 +0530

    danger

--- src/actions/getCurrentUser.ts
+++ src/actions/getCurrentUser.ts
@@ -1,4 +1,4 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "../app/api/auth/[...nextauth]/route";
 import prisma from "@/vendor/db";
 import { getServerSession } from "next-auth";
 
