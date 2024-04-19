e4325523a4b08158ebff585bb11978c5f5152183
tyson-tyler <benword25@gmail.com>
2024-04-19 09:01:21 +0530

    asfd

--- src/app/(routes)/subscriptions/page.tsx
+++ src/app/(routes)/subscriptions/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useState } from "react";
 import getSubscriptionVideos from "@/actions/getSubscriptionVideo";
 import SubscriptionList from "@/components/subscription/Subscription";
