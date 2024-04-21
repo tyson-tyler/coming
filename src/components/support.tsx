732b22c544881aa67cb897def36b9051bbb6d0bb
tyson-tyler <benword25@gmail.com>
2024-04-21 12:35:26 +0530

    hello

--- src/components/support.tsx
+++ src/components/support.tsx
@@ -7,8 +7,8 @@ const Support = () => {
     <>
       <div className="flex justify-center items-center flex-col">
         <h1 className="head_text text-center pt-5 text-white w-full pb-5">
-          Thanks for Visting{" "}
-          <span className="usespan text-center mb-4">at MyPager.</span>
+          Watch and Learn how{" "}
+          <span className="usespan text-center mb-4">use Myaimix</span>
         </h1>
         <Link href={"/"}>
           <Button className="p-4 rounded-md my-3">Go to Website 🌴</Button>
