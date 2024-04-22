416d86714fa50fe2dcdfe907f89ecaccb3e414f0
tyson-tyler <benword25@gmail.com>
2024-04-22 23:10:40 +0530

    jkhjghg

--- src/app/(routes)/creator/page.tsx
+++ src/app/(routes)/creator/page.tsx
@@ -1,13 +1,16 @@
 import getTreadingVideo from "@/actions/NewCreator";
 import LeftBar from "@/components/Leftbar";
 import VideoCard from "@/components/shared/VideoCard";
+import Subscription from "../subscriptions/page";
+import getCurrentSubscription from "@/actions/getCurrentSubscriptions";
 
 export default async function Home() {
   const trendingVideos = await getTreadingVideo();
+  const subscriptions = await getCurrentSubscription();
   return (
     <div className="w-full relative  mt-16 flex justify-center">
       <div className="sm:hidden md:flex flex flex-between md:mr-4">
-        <LeftBar />
+        <LeftBar subscribedChannels={subscriptions} />
       </div>
       <div className="basis-[85%] sm:mb-[100px] lg:mb-[0px] gap-x-10 gap-y-10 mt-5 justify-center grid-container">
         {trendingVideos
