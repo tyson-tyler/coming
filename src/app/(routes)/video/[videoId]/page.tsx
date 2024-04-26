fd3220d564ddff6e6481c09bf7c134e652a31986
tyson-tyler <benword25@gmail.com>
2024-04-26 14:51:41 +0530

    nice

--- src/app/(routes)/video/[videoId]/page.tsx
+++ src/app/(routes)/video/[videoId]/page.tsx
@@ -27,31 +27,33 @@ export default async function VideoPage({
   });
   const recommendedVideos = await getRecommendedVideos({ video });
   return video && channel && comments ? (
-    <div className="flex flex-col lg:flex-row mx-6 gap-4 mb-[100px] mt-10">
+    <div className="flex flex-col  sm:mx-6 gap-4 mb-[100px] mt-10">
       <div className="w-full flex flex-col gap-4">
         <VideoPlayer videoSrc={video.videoSrc} />
-        <LikeSubscribePage video={video} channel={channel} />
-        <h1 className="text-2xl my-2 pb-2 font-semibold break-all dark:text-white text-black">
-          {video.title}
-          <Description video={video} />
-        </h1>
+        <div className="mx-2">
+          <LikeSubscribePage video={video} channel={channel} />
+          <h1 className="text-2xl my-2 pb-2 font-semibold break-all dark:text-white text-black">
+            {video.title}
+            <Description video={video} />
+          </h1>
 
-        <div className="w-full grid-container gap-4 pb-4">
-          {recommendedVideos
-            ? recommendedVideos.map((recommendedVideo) => {
-                return (
-                  <VideoCard
-                    key={recommendedVideo.id}
-                    isVertical={false}
-                    video={recommendedVideo}
-                    channel={recommendedVideo.channel}
-                    channelAvatar={channel.imageSrc}
-                  />
-                );
-              })
-            : null}
+          <div className="w-full grid-container gap-4 pb-4">
+            {recommendedVideos
+              ? recommendedVideos.map((recommendedVideo) => {
+                  return (
+                    <VideoCard
+                      key={recommendedVideo.id}
+                      isVertical={false}
+                      video={recommendedVideo}
+                      channel={recommendedVideo.channel}
+                      channelAvatar={channel.imageSrc}
+                    />
+                  );
+                })
+              : null}
+          </div>
+          <CommentSection comments={comments} videoId={video.id} />
         </div>
-        <CommentSection comments={comments} videoId={video.id} />
       </div>
     </div>
   ) : (
